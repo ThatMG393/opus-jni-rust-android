8240f42e89dbe847b1572029a9621c25b4b9b49b
Apehum <apehumchik@gmail.com>
2023-10-13 17:31:13 +0800

    test: throw exception

--- src/test/java/com/plasmoverse/opus/OpusTest.java
+++ src/test/java/com/plasmoverse/opus/OpusTest.java
@@ -5,25 +5,20 @@ import org.junit.jupiter.api.Test;
 public final class OpusTest {
 
     @Test
-    public void decode() {
-        try {
-            OpusEncoder encoder = OpusEncoder.create(48_000, false, 960, OpusMode.VOIP);
+    public void decode() throws Exception {
+        OpusEncoder encoder = OpusEncoder.create(48_000, false, 960, OpusMode.VOIP);
 
-            byte[] encoded = encoder.encode(new short[960]);
+        byte[] encoded = encoder.encode(new short[960]);
 
-            encoder.setBitrate(50_000);
-            encoder.reset();
-            encoder.close();
+        encoder.setBitrate(50_000);
+        encoder.reset();
+        encoder.close();
 
-            OpusDecoder decoder = OpusDecoder.create(48_000, false, 1200);
+        OpusDecoder decoder = OpusDecoder.create(48_000, false, 1200);
 
-            short[] decoded = decoder.decode(encoded);
+        short[] decoded = decoder.decode(encoded);
 
-            decoder.reset();
-            decoder.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        decoder.reset();
+        decoder.close();
     }
 }
